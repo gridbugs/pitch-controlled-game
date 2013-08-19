49d514bb1fd3f3a9b0a6062726571d0908ad80a9
Stephen Sherratt <steve@stevebob.net>
2013-08-20 03:05:05 +1000

    added documentation to the mic script

--- mic.py
+++ mic.py
@@ -36,7 +36,7 @@ def highest_point(data):
 
 pygame.init()
 surface = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Hello, World!')
+pygame.display.set_caption('Microphone Pitch')
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -50,33 +50,44 @@ while True:
 
     try:
 
+        # get some data from the mic as a string
         data_str = stream.read(CHUNK)
+
+        # convert the string to an integer array representing amplitude over time
         data_time_domain = fromstring(data_str, dtype=short)
+
+        # just use the left channel
         data_l = data_time_domain[1::2]
+
+        # discrete fourier transform to convert the data into a complex number array
+        # representing the amplidue of the different frequencies in the sample
         data_freq_domain = fft(data_l)[:len(data_l)/2]
 
-        surface.fill(BLACK)
 
-        freq = map(lambda x: x/1000, map(int, map(abs, data_freq_domain)))
-        interpolated = log_interpolate(
-            map(lambda x: max(x, 300) - 300,
-            map(lambda x:
-              math.log(x+1)*40,
-              freq
-            )),
-            map(lambda x: RES * x, range(WIDTH/RES)),
-            110
+        interpolated =
+          log_interpolate(                 # evenly distribute values on the x axis
+          map(lambda x: max(x, 300) - 300, # filter out low amplitudes
+          map(lambda x: math.log(x+1)*40,  # evenly distribute values on the y axis
+          map(abs, data_freq_domain))),    # get the absolute value of each frequency domain value
+          map(lambda x: RES * x, range(WIDTH/RES)), # x axis points we're interested in
+          110   # scale in the x axis
         )
 
-        best = highest_point(interpolated)
-        if best[1] > 80 and best[1] > 80:
-          x = (best[0] - 80) * 4
-          y = best[1] * 2
+        # clear the screen
+        surface.fill(BLACK)
+
+        best = highest_point(interpolated) # find the loudest frequency
+        if best[0] > 80 and best[1] > 80:  # filter out low frequencies quiet noises
+          x = (best[0] - 80) * 4           # horizontal scaling
+          y = best[1]
+
+          # draw the line
           pygame.draw.line(surface, (255, 255, 255),
             (x, HEIGHT), (x, HEIGHT-y), 5)
 
         pygame.display.update()
 
     except IOError:
+        # skip over errors
         pass
 
